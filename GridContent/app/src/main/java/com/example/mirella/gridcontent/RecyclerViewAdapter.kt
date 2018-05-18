096e763c1c7f4276b8edc9ff85276757c29461fc
DESKTOP-A5KE19V\Mirella <mirella.lds@gmail.com>
2018-05-18 00:16:10 -0300

    init

---
+++ GridContent/app/src/main/java/com/example/mirella/gridcontent/RecyclerViewAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.mirella.gridcontent
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+
+class RecyclerViewAdapter(var data: MutableList<Report>): RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        var view: View
+        var inflater: LayoutInflater = LayoutInflater.from(parent.context)
+        view = inflater.inflate(R.layout.image_card_view, parent, false)
+        return MyViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.image.setImageDrawable()
+    }
+
+
+    class MyViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
+        val image = itemView!!.findViewById<ImageView>(R.id.image_view)
+
+    }
+
+}
---
+++ GridContent/app/src/main/java/com/example/mirella/gridcontent/Report.kt
@@ -0,0 +1,3 @@
+package com.example.mirella.gridcontent
+
+data class Report(var image: String)
